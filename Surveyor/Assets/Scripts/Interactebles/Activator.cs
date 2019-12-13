10d5ab8989e212c602123049db63d4a94e398591
loljoch <loljoch@gmail.com>
2019-12-13 17:17:23 +0100

    Working pressure plate, doors, tilt mechanic

---
+++ Surveyor/Assets/Scripts/Interactebles/Activator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Activator : MonoBehaviour
+{
+    public UnityEvent onTriggerEnter;
+    public bool triggered = false;
+    public bool triggerOnce = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        onTriggerEnter?.Invoke();
+
+        if (triggerOnce)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
