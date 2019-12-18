79b7cc963b475aa16c86703b54e9326c19c5e17f
Ruben Hooijer <loljoch@gmail.com>
2019-12-18 20:37:28 +0100

    DarkcentenLevelTechDone

---
+++ Surveyor/Assets/Scripts/Interactebles/SceneSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField] private AudioClip wooshSound;
+    [SerializeField] private AudioSource audioSource;
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SwitchToDarkScene();
+        }
+    }
+    
+    public void SwitchToDarkScene()
+    {
+        audioSource.PlayOneShot(wooshSound);
+        SceneManager.LoadScene("DocentenLevel_Phase2", LoadSceneMode.Single);
+    }
+}
