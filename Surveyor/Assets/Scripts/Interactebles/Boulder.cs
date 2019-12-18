3bd32aec202355362b1e32b9e18520d7c1320638
Ruben Hooijer <loljoch@gmail.com>
2019-12-18 15:26:06 +0100

    Fixed platform prefabs and boulder sounds

--- Surveyor/Assets/Scripts/Interactebles/Boulder.cs
+++ Surveyor/Assets/Scripts/Interactebles/Boulder.cs
@@ -2,4 +2,24 @@
 
 public class Boulder : MonoBehaviour
 {
+    [SerializeField] private AudioClip rollingAudio, onImpact;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Rigidbody rb;
+
+    private void FixedUpdate() 
+    {
+        if(rb.velocity.x + rb.velocity.z > 0)
+        {
+            audioSource.clip = rollingAudio;
+            audioSource.Play();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(rb.velocity.x + rb.velocity.z > 0)
+        {
+            audioSource.PlayOneShot(onImpact);
+        }
+    }
 }
